9661680fd3b398ca0beac6992a3766acb55d961b
dgranadeabreu <sstargatte@gmail.com>
2015-04-07 19:24:29 +0200

    Initial commit

---
+++ academy/models.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+from openerp import models, fields, api
+from openerp import models, fields, api
+
+class Teachers(models.Model):
+    _name = 'academy.teachers'
+
+    name = fields.Char()
+# class academy(models.Model):
+#     _name = 'academy.academy'
+
+#     name = fields.Char()
